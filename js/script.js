7b84e2c140ffb3766de64177001e8faacfef8830
Wojciech Dyga <wojtek.dyga2@wp.pl>
2022-02-04 14:15:00 +0100

    Add three wievs

---
+++ js/script.js
@@ -0,0 +1,91 @@
+
+
+function button1 (){
+    document.getElementById('first').classList.remove('active');
+    document.getElementById('second').classList.add('active');
+}
+
+document.getElementById('first').addEventListener('click', function(e){
+    e.preventDefault();
+    button1();
+})
+
+function button2 (){
+    document.getElementById('second').classList.remove('active');
+    document.getElementById('third').classList.add('active');
+    
+}
+document.getElementById('test-button').addEventListener('click', function(e){
+    e.preventDefault();
+    button2();
+})
+
+
+function button3 (){
+    document.getElementById('third').classList.remove('active');
+    document.getElementById('first').classList.add('active');
+    
+}
+document.getElementById('fourth').addEventListener('click', function(e){
+    e.preventDefault();
+    button3();
+})
+
+function button4 (){
+    document.getElementById('third').classList.remove('active');
+    document.getElementById('second').classList.add('active');
+    
+}
+document.getElementById('fifth').addEventListener('click', function(e){
+    e.preventDefault();
+    button4();
+})
+
+
+
+
+
+
+
+/*
+
+//MODAL
+
+//zamykanie modala
+function closeModal() {
+    document.getElementById('overlay').classList.remove('show')
+}
+//podpięcie pod przyciski zamykające
+document.querySelectorAll('#overlay .js--close-modal').forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+      e.preventDefault()
+      closeModal()
+    })
+})
+//otwarcie modala
+
+function openModal(modal) {
+    document.querySelectorAll('#overlay > *').forEach(function(modal) {
+      modal.classList.remove('show')
+    })
+    document.querySelector('#overlay').classList.add('show');
+    document.querySelector(modal).classList.add('show')
+}
+
+document.getElementById('test-button').addEventListener('click', function(e) {
+    e.preventDefault();
+    openModal('#myModal');
+});
+
+function button3 (){
+    document.getElementById('overlay').classList.remove('show');
+    document.querySelector('.modal').classList.remove('show')
+    document.getElementById('first').classList.active('active');
+    
+}
+
+document.querySelector('.btn-3').addEventListener('click', function(e){
+    e.preventDefault();
+    button3();
+})
+*/
